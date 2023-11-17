1d16083593868c241db11f505119e134c689cb44
ʟᴇᴀ <xealea@proton.me>
2023-11-17 14:51:57 +0700

    [autostart] test script for suspend on tty1

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -23,11 +23,11 @@ dunst -config ~/.config/dunst/dunstrc &
 # X lock
 # Check if systemd is in use
 if [ -d /run/systemd/system ]; then
-    exec xautolock -time 5 -locker "qtile cmd-obj -o cmd -f shutdown" -detectsleep &
+    exec xautolock -time 5 -locker "qtile cmd-obj -o cmd -f shutdown" -detectsleep && systemctl suspend &
 # Check if runit is in use
 elif [ -d /etc/runit ]; then
     # Add runit-specific command here
-    exec xautolock -time 5 -locker "qtile cmd-obj -o cmd -f shutdown" -detectsleep &
+    exec xautolock -time 5 -locker "qtile cmd-obj -o cmd -f shutdown" -detectsleep && loginctl suspend &
 else
     echo "Unknown init system"
 fi
