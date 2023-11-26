a3212c6f159e6c4f51e11c026027cd391712fcbd
xealea <xealea@proton.me>
2023-11-26 13:34:53 +0700

    update
    Signed-off-by: xealea <xealea@proton.me>

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
 # Email: xealea@proton.me
 
+# Kill already running process
+_ps=(xsettingsd ksuperkey)
+for _prs in "${_ps[@]}"; do
+	if [[ `pidof ${_prs}` ]]; then
+		killall -9 ${_prs}
+	fi
+done
+
+# polkit agent
+if [[ ! `pidof xfce-polkit` ]]; then
+	/usr/lib/xfce-polkit/xfce-polkit &
+fi
+
 # Run processes
-exec ~/.fehbg &
-exec xfsettingsd &
-exec xfce4-power-manager &
-exec xdg-user-dirs-update &
-exec xdg-user-dirs-gtk-update &
-exec dbus-run-session pipewire &
-exec /usr/lib/xfce-polkit/xfce-polkit &
+~/.fehbg &
+xfsettingsd &
+xfce4-power-manager &
+xdg-user-dirs-update &
+xdg-user-dirs-gtk-update &
+dbus-run-session pipewire &
+/usr/lib/xfce-polkit/xfce-polkit &
+
+# Enable Super Keys For Menu
+ksuperkey -e 'Super_L=Alt_L|F1' &
+ksuperkey -e 'Super_R=Alt_L|F1' &
 
 # Start dunst & picom
-exec picom --config  ~/.config/picom.conf &
-exec dunst -config ~/.config/dunst/dunstrc &
+picom --config  ~/.config/picom.conf &
+dunst -config ~/.config/dunst/dunstrc &
 
 # X lock
-exec xscreensaver -no-splash &
-exec xautolock -time 5 -locker "xscreensaver-command -lock" -detectsleep &
+xscreensaver -no-splash &
+xautolock -time 5 -locker "xscreensaver-command -lock" -detectsleep &
