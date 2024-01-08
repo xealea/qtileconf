a12c0ad7631343da2e6831d6c384c6b98a0721eb
xealea <xealea@proton.me>
2024-01-08 14:21:48 +0700

    up
    Signed-off-by: xealea <xealea@proton.me>

--- .qtheme/default/qtile/config.py
+++ .qtheme/default/qtile/config.py
@@ -20,14 +20,23 @@ keys = [
     Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
-    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
-    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
+    Key(
+        [mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"
+    ),
+    Key(
+        [mod, "shift"],
+        "l",
+        lazy.layout.shuffle_right(),
+        desc="Move window to the right",
+    ),
     Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
     Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
+    Key(
+        [mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"
+    ),
     Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
@@ -51,24 +60,38 @@ keys = [
         lazy.window.toggle_fullscreen(),
         desc="Toggle fullscreen on the focused window",
     ),
-    Key([mod], "t", lazy.window.toggle_floating(), desc="Toggle floating on the focused window"),
+    Key(
+        [mod],
+        "t",
+        lazy.window.toggle_floating(),
+        desc="Toggle floating on the focused window",
+    ),
     Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-#    Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
-
+    #    Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
     # Misc and my custom cmd
     Key([mod], "x", lazy.spawn("rofi -show drun"), desc="Spawn a command launcher"),
-    Key([], "XF86AudioRaiseVolume", lazy.spawn("pamixer -i 10"), desc='Volume Up'),
-    Key([], "XF86AudioLowerVolume", lazy.spawn("pamixer -d 10"), desc='Volume Down'),
-    Key([], "XF86AudioMute", lazy.spawn("pamixer -t"), desc='Volume Mute'),
-    Key([], "XF86AudioPlay", lazy.spawn("playerctl play-pause"), desc='playerctl'),
-    Key([], "XF86AudioPrev", lazy.spawn("playerctl previous"), desc='playerctl'),
-    Key([], "XF86AudioNext", lazy.spawn("playerctl next"), desc='playerctl'),
-    Key([], "XF86MonBrightnessUp", lazy.spawn("brightnessctl s 10%+"), desc='Brightness Up'),
-    Key([], "XF86MonBrightnessDown", lazy.spawn("brightnessctl s 10%-"), desc='Brightness Down'),
-    Key([mod], "e", lazy.spawn("thunar"), desc='File manager'),
-    Key([mod], "h", lazy.spawn("xclip"), desc='Clipboard'),
-    Key([mod], "s", lazy.spawn("screenshot"), desc='Screenshot'),
+    Key([], "XF86AudioRaiseVolume", lazy.spawn("control volume +5"), desc="Volume Up"),
+    Key([], "XF86AudioLowerVolume", lazy.spawn("control volume -5"), desc="Volume Down"),
+    Key([], "XF86AudioMute", lazy.spawn("control mute x"), desc="Volume Mute"),
+    Key([], "XF86AudioPlay", lazy.spawn("playerctl play-pause"), desc="playerctl"),
+    Key([], "XF86AudioPrev", lazy.spawn("playerctl previous"), desc="playerctl"),
+    Key([], "XF86AudioNext", lazy.spawn("playerctl next"), desc="playerctl"),
+    Key(
+        [],
+        "XF86MonBrightnessUp",
+        lazy.spawn("control brightness 5+"),
+        desc="Brightness Up",
+    ),
+    Key(
+        [],
+        "XF86MonBrightnessDown",
+        lazy.spawn("control brightness 5-"),
+        desc="Brightness Down",
+    ),
+    Key([mod], "e", lazy.spawn("thunar"), desc="File manager"),
+    Key([mod], "h", lazy.spawn("xclip"), desc="Clipboard"),
+    Key([mod], "s", lazy.spawn("screenshot"), desc="Screenshot"),
     Key([mod], "t", lazy.spawn("change"), desc="Theme Change"),
 ]
 
